b1b63412735dd32be5706e7fc3259671a4dd3dda
kokapelli <hampus.falk@hotmail.se>
2019-04-02 17:14:21 +0200

    Entities can now evolve and mutate. They have now received a graphical representation of vision

--- Models/Entity.js
+++ Models/Entity.js
@@ -1,8 +1,13 @@
+let mr = 0.01; // Mutation Rate
+
 class Entity{
-  constructor(){
+  // TODO
+  // Make eating a dynamic activity. Should take as long as the food is big
+  // Poison resistance makes poison food?
+  constructor(x, y, dna){
       this.acceleration = createVector();
-      this.velocity = createVector();
-      this.position = createVector(random(width), random(height));
+      this.velocity = p5.Vector.random2D();
+      this.position = createVector(x, y);
       this.r = 6;
       this.maxSpeed = 5;
       this.maxForce = 0.5;
@@ -10,13 +15,69 @@ class Entity{
       this.intimidation = random(255)
 
       this.dna = [];
-      this.dna[0] = random(-5, 5);
-      this.dna[1] = random(-5, 5);
+      //TODO
+      //Change dna to a range between 0 and 1
+      if(dna == undefined){
+        this.dna[0] = random(-2, 2); //Food Weight
+        this.dna[1] = random(-2, 2); //Poison Weight
+        this.dna[2] = random(10, 100); //Food Perception
+        this.dna[3] = random(10, 100); //Poison Perception       
+    } else {
+      //Mutation
+      this.dna[0] = dna[0];
+      if(random(1) < mr){
+        this.dna[0] += random(-0.1, 0.1);
+      }
+      this.dna[1] = dna[1];
+      if(random(1) < mr){
+        this.dna[1] += random(-0.1, 0.1);
+      }
+      this.dna[2] = dna[2];
+      if(random(1) < mr){
+        this.dna[1] += random(-10, 10);
+      }
+      this.dna[3] = dna[3];
+      if(random(1) < mr){
+        this.dna[1] += random(-10, 10);
+      }
+    }
+  }
+
+
+  clone(){
+    if(random(1) < 0.001) {
+    return new Entity(this.position.x, this.position.y, this.dna);
+    } else {
+      return null;
+    }
+}
+
+  boundaries(){
+    let d = 10;
+    let desired = null;
+    if (this.position.x < d) {
+      desired = createVector(this.maxSpeed, this.velocity.y);
+    } else if (this.position.x > width - d) {
+      desired = createVector(-this.maxSpeed, this.velocity.y);
+    }
+  
+    if (this.position.y < d) {
+      desired = createVector(this.velocity.x, this.maxSpeed);
+    } else if (this.position.y > height - d) {
+      desired = createVector(this.velocity.x, -this.maxSpeed);
+    }
+  
+    if (desired !== null) {
+      desired.setMag(this.maxSpeed);
+      let steer = p5.Vector.sub(desired, this.velocity);
+      steer.limit(this.maxForce);
+      this.applyForce(steer);
+    }
   }
 
   behaviors(good, bad){
-    let steerG = this.eat(good, 0.4);
-    let steerB = this.eat(bad, -0.5);
+    let steerG = this.eat(good, 0.4, this.dna[2]);
+    let steerB = this.eat(bad, -0.5, this.dna[3]);
 
     steerG.mult(this.dna[0]);
     steerB.mult(this.dna[1]);
@@ -25,41 +86,31 @@ class Entity{
     this.applyForce(steerB);
   }
 
-  eat(list, nutrition){
+  eat(list, nutrition, perception){
     let record = Infinity;
-    let closest = -1;
-    for(let i = 0; i < list.length; i++){
+    let closest = null;
+    for(let i = list.length-1; i >= 0; i--){
       let d = this.position.dist(list[i])
-      if(d < record){
-        record = d;
-        closest = i;
+      if(d < this.maxSpeed) {  //maxSpeed used so the entity does not jump over the object
+        list.splice(i, 1);
+        this.health += nutrition;
+      } else {      
+        if(d < record && d < perception){
+          record = d;
+          closest = list[i];
+        }
       }
-    }
-    if(record < 5){
-      list.splice(closest, 1);
-      this.health += nutrition;
-    } else if (closest > - 1){
-      return this.seek(list[closest]);
+    } 
+
+  if (closest != null){
+    return this.seek(closest);
   }
 
   return createVector(0, 0);
 }
 
   dead() {
-      return (this.health < 0 ||
-          this.position.x > width + this.r ||
-          this.position.x < -this.r ||
-          this.position.y > height + this.r ||
-          this.position.y < -this.r
-      );
-  }
-
-  update() {
-    this.health -= 0.01;
-    this.velocity.add(this.acceleration);
-    this.velocity.limit(this.maxSpeed);
-    this.position.add(this.velocity);
-    this.acceleration.mult(0);
+      return this.health < 0;
   }
 
   applyForce(force) {
@@ -74,6 +125,14 @@ class Entity{
     steer.limit(this.maxForce);
 
     return steer;
+  }
+
+  update() {
+    this.health -= 0.005;
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
   }
 
   display() {
@@ -99,8 +158,6 @@ class Entity{
       rect(x, y, w*this.health, h);
 
 
-
-
       pop()
 
       //Draw Entity
@@ -109,13 +166,26 @@ class Entity{
       translate(this.position.x, this.position.y);
       rotate(angle);
       
-      //Draw Desire Line
-      stroke(0, 255, 0);
-      line(0, 0, 0, -this.dna[0] * 20)
-      stroke(255, 0, 0);
-      line(0, 0, 0, -this.dna[1] * 20)
-      
-      // Draw Creature
+      if(debug.checked()) {
+        noFill();
+        stroke(0, 255, 0, 100);
+        ellipse(0, 0, this.dna[2] * 2)
+        line(0, 0, 0, -this.dna[0] * 20)
+
+        stroke(255, 0, 0, 100);
+        ellipse(0, 0, this.dna[3] * 2)
+        line(0, 0, 0, -this.dna[1] * 20)
+
+        //Vision
+        stroke(255, 255, 255, 100);
+        fill(255, 255, 255, 100);
+        strokeWeight(4)
+        // Decreasing Start and increasing End increases field
+        // Start -180 and End 0 equals front 180 vision
+        // width and height determines range
+        arc(0, 0, 180, 180, radians(-160), radians(-20)); 
+      }
+
       fill(this.intimidation)
       stroke(200)
       strokeWeight(1)
--- sketch.js
+++ sketch.js
@@ -3,9 +3,13 @@ let food = [];
 let poison = [];
 let superFood = null;
 
+let debug;
 
 function setup() {
-    createCanvas(windowWidth * 0.95, windowHeight * 0.95);
+    let canvas = createCanvas(windowWidth * 0.95, windowHeight * 0.95);
+
+    canvas.parent('canvascontainer');
+    debug = select('#debug');
 
     for(let i = 0; i < 50; i++){
         let x = random(width);
@@ -21,7 +25,9 @@ function setup() {
 
     // Create initial population
     for (let i = 0; i < 20; i++) {
-        population[i] = new Entity();
+        let x = random(width);
+        let y = random(height);
+        population[i] = new Entity(x, y);
     }
 
     superFood = new Food(random(width), random(height));
@@ -29,6 +35,13 @@ function setup() {
 
 function draw() {
     background(51);
+
+    if(random(1) <= 0.05){
+        let x = random(width);
+        let y = random(height);
+        food.push(createVector(x, y));
+    }
+
 
     for(var i = 0; i < food.length; i++){
         fill(0, 255, 0);
@@ -44,15 +57,27 @@ function draw() {
 
 
     for(let i = population.length-1; i >= 0; i--){
+        population[i].boundaries();
         population[i].behaviors(food, poison);
         population[i].update();
         population[i].display();
 
+        //Spawn clones
+        let newEntity = population[i].clone();
+        if(newEntity != null){
+            population.push(newEntity);
+        }
+
         if(population[i].dead()){
+            let x = population[i].position.x;
+            let y = population[i].position.y;
+            food.push(createVector(x, y));
             population.splice(i, 1);
         }
     }
 
     superFood.update()
     superFood.display()
+
+
 }
